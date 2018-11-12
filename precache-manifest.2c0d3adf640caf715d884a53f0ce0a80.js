cb293c22ba4d8efccf2b4f0b0baecf90ef1013f2
gmarkosov <george32451@mail.ru>
2018-11-12 17:25:46 +0300

    Updates

---
+++ precache-manifest.2c0d3adf640caf715d884a53f0ce0a80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e45f83a15a29ad01e321",
+    "url": "/demo-routing-app/static/js/runtime~main.e45f83a1.js"
+  },
+  {
+    "revision": "c4fb046da16a9cf409ac",
+    "url": "/demo-routing-app/static/js/main.c4fb046d.chunk.js"
+  },
+  {
+    "revision": "7ac7b7101ffb088ece50",
+    "url": "/demo-routing-app/static/js/1.7ac7b710.chunk.js"
+  },
+  {
+    "revision": "c4fb046da16a9cf409ac",
+    "url": "/demo-routing-app/static/css/main.24c61ce8.chunk.css"
+  },
+  {
+    "revision": "51274908440e0bd732ed85b2a686c1b5",
+    "url": "/demo-routing-app/index.html"
+  }
+];
